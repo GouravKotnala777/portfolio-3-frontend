db19c1b91652ce6c3945ee22f50528da416f7ea3
gouravkotnala <gouravkotnala777@gmail.com>
2026-01-23 15:47:01 +0530

    style : update icon related styles

--- src/components/primary/Header.component.tsx
+++ src/components/primary/Header.component.tsx
@@ -1,23 +1,62 @@
 import { NavLink } from "react-router-dom";
 import useTheme from "../../hooks/useTheme";
+import { useEffect, useState } from "react";
 
 
 function Header() {
     const {theme, toggleTheme} = useTheme();
-    
+    const [isHeaderRaised, setIsHeaderRaised] = useState<boolean>(false);
+    const [logoYPosition, setLogoYPosition] = useState<number>(0);
+
+
+    function raiseHeaderHandler() {
+        const currentScrollY = window.scrollY;
+        if (currentScrollY >= 70) {
+            setIsHeaderRaised(true);
+        }
+        else{
+            setIsHeaderRaised(false);
+        }
+        if (currentScrollY < 264) {
+            setLogoYPosition(-100); 
+        }
+        else if (currentScrollY >= 264 && currentScrollY < 422) {
+            setLogoYPosition(((currentScrollY-262)/160)*100);
+        }
+        else{
+            setLogoYPosition(100);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", raiseHeaderHandler);
+
+        return() => window.removeEventListener("scroll", raiseHeaderHandler);
+    }, []);
+        
     return(
-        <div className="bg-white dark:bg-neutral-900 fixed top-0 left-0 w-full pt-2 z-20">
+        <header className="bg-white dark:bg-neutral-900 fixed top-0 left-0 w-full pt-2 z-20">
             <div className="border-y border-neutral-100 dark:border-neutral-800">
-                <div className="border-x border-neutral-100 dark:border-neutral-800 max-w-3xl mx-auto ">
-                    <div className="flex justify-between items-center p-3">
-                        <div className="text-neutral-700 dark:text-neutral-200">Logo</div>
+                <div className={`border-x border-neutral-100 dark:border-neutral-800 max-w-3xl mx-auto transition-all ease-in-out duration-500
+                    ${isHeaderRaised&&"[box-shadow:0px_3px_20px_0.2px_var(--color-neutral-300)] dark:[box-shadow:0px_3px_20px_0.2px_var(--color-neutral-700)]"}
+                `}>
+                    <div className="flex justify-between items-center">
+                        <div className="text-neutral-700 dark:text-neutral-200">
+                            <div className="size-12 [box-shadow:0px_0px_1px_0px_#00000020_inset] dark:[box-shadow:0px_0px_1px_0px_#ffffff20_inset] bg-a rounded-full overflow-hidden relative">
+                                <img src="gourav-kotnala1.png" alt="gourav-kotnala1.png" className={`z-2 absolute left-0 h-full w-full [box-shadow:0px_0px_1px_0px_#00000050] dark:[box-shadow:0px_0px_1px_0px_#ffffff50] rounded-full bg-linear-0 from-neutral-500 to-white dark:from-neutral-50 dark:to-neutral-500`}
+                                    style={{
+                                        top:`${100-logoYPosition}%`
+                                    }}
+                                />
+                            </div>
+                        </div>
                         <div className="flex items-center gap-6 text-neutral-700 dark:text-neutral-200">
                             {
-                                ["Portfolio", "Components", "Blog"].map((navitem) => (
-                                    <NavLink to={"#######"}>{navitem}</NavLink>
+                                [{name:"Portfolio", url:"/"}, {name:"Components", url:"/components"}, {name:"Projects", url:"/projects"}].map(({name, url}) => (
+                                    <NavLink to={url}>{name}</NavLink>
                                 ))
                             }
-                            <button className="border border-neutral-100 dark:border-neutral-700 [box-shadow:0px_0px_1px_0.1px_#00000010_inset] dark:[box-shadow:0px_0px_1px_0.1px_#ffffff10_inset] py-1 px-3 rounded-2xl flex justify-between items-center gap-1">
+                            <button className="border border-neutral-100 dark:border-neutral-800 [box-shadow:0px_0px_2px_0.1px_var(--color-neutral-200)_inset] dark:[box-shadow:0px_0px_2px_0.1px_var(--color-neutral-700)_inset] py-1 my-2 px-3 rounded-2xl flex justify-between items-center gap-1">
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor"
                                         className="size-4 text-neutral-500 dark:text-neutral-400"
@@ -55,7 +94,7 @@ function Header() {
                     </div>
                 </div>
             </div>
-        </div>
+        </header>
     )
 };
 
