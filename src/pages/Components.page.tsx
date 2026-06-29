00fa15ae6f280fd839142b31da21a99ef489fd21
gouravkotnala <gouravkotnala777@gmail.com>
2026-06-29 17:01:14 +0530

    fix(Components.page.tsx) : src/pages/Components.page.tsx(8,44): error TS2307: Cannot find module '../components/hidden/SlitherInput' or its corresponding type declarations.

--- src/pages/Components.page.tsx
+++ src/pages/Components.page.tsx
@@ -5,7 +5,16 @@ import { DIA_SCROLLER_NAVBAR_CODE, FLUID_NAVBAR_CODE, GOOEY_NAVBAR_CODE, METEOR_
 import useTheme from "../hooks/useTheme";
 import CodeBlock from "../components/reusable/CodeBlock.component";
 import { useLocation } from "react-router-dom";
-import type { SlitherAnimationTypes } from "../components/hidden/SlitherInput";
+
+type ThicknessPropTypes = 1|1.1|1.2|1.3|1.4|1.5|1.6|1.7|1.8|1.9|2|2.1|2.2|2.3|2.4|2.5|2.6|2.7|2.8|2.9|3|3.2|3.3|3.5|3.7|3.9|4|4.5|4.9|5|5.3|5.5|5.7;
+interface SlitherAnimationTypes{
+    waveLength?:"xs"|"sm"|"md"|"lg"|"xl"|"xxl";
+    amplitude?:1|2|3|4|5|6|7|8|9;
+    smoothness?:1|2|3|4|5|6|7|8|9;
+    waveThickness?:ThicknessPropTypes;
+    blurEffect?:boolean;
+    shrinkEffect?:boolean;
+};
 
 const CODE_ICON = () => {
     return(
