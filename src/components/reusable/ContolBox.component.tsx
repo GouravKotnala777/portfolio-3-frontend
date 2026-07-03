2c3899e6d74af8869e86fe86f16c6059866ddd2e
gouravkotnala <gouravkotnala777@gmail.com>
2026-07-03 16:02:36 +0530

    feat(ContolBox.component.tsx) : add ContolBox component to show component props working

---
+++ src/components/reusable/ContolBox.component.tsx
@@ -0,0 +1,68 @@
+import type { Dispatch, HTMLInputTypeAttribute, SetStateAction } from "react";
+
+export type InputTypes={name:string; type:HTMLInputTypeAttribute|"select"; options:(string|number|boolean)[];};
+
+interface ControlBoxPropTypes<ControllerStateType>{
+    inputs:InputTypes[];
+    controlState:ControllerStateType;
+    setControlState:Dispatch<SetStateAction<ControllerStateType>>;
+    isAnimationRunning:boolean;
+};
+
+
+
+function ControlBox<ControllerStateType>({inputs, controlState, setControlState, isAnimationRunning}:ControlBoxPropTypes<ControllerStateType>) {
+
+    function hexToRgb(hex: string): string {
+        hex = hex.replace("#", "");
+
+        const r = parseInt(hex.slice(0, 2), 16);
+        const g = parseInt(hex.slice(2, 4), 16);
+        const b = parseInt(hex.slice(4, 6), 16);
+
+        return `${r},${g},${b}`;
+    }
+    
+    return(
+        <div className={`w-55 border border-gray-300 dark:border-gray-700 text-gray-400 dark:text-gray-400 text-[12px] py-2 px-3 rounded-sm font-mono font-semibold bg-gray-100 dark:bg-gray-800 ${isAnimationRunning?"opacity-50":"opacity-100"} transition-opacity ease-in-out duration-300 z-1`}>
+            {
+                inputs.map((input, index) => {
+                    if (input.type === "text" || input.type === "number") {
+                        return(
+                            <div key={index} className="flex justify-between">
+                                <label htmlFor={input.name}>{input.name}</label>
+                                <input id={input.name} type={input.type} name={input.name} disabled={isAnimationRunning} onChange={(e) => setControlState({...controlState, [e.target.name]:e.target.value})} />
+                            </div>
+                        )
+                        
+                    }
+                    else if (input.type === "color") {
+                        return(
+                            <div key={index} className="flex justify-between">
+                                <label htmlFor={input.name}>{input.name}</label>
+                                <input id={input.name} type={input.type} name={input.name} value={`rgb(${controlState[input.name as keyof ControllerStateType]})`} disabled={isAnimationRunning} onChange={(e) => setControlState({...controlState, [e.target.name]:hexToRgb(e.target.value)})} />
+                            </div>
+                        )
+                        
+                    }
+                    else if (input.type === "select") {
+                        return(
+                            <div key={index} className="flex justify-between">
+                                <label htmlFor={input.name}>{input.name}</label>
+                                <select id={input.name} defaultValue={JSON.stringify(controlState[input.name as keyof ControllerStateType])} name={input.name} disabled={isAnimationRunning} onChange={(e) => setControlState({...controlState, [e.target.name]:e.target.value})}>
+                                    {
+                                        input.options.map((opt, optInd) => (
+                                            <option key={`${opt}-${optInd}`}>{opt}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
+                        )
+                    }
+                })
+            }
+        </div>
+    )
+};
+
+export default ControlBox;
